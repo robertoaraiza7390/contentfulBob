c2e241ec2d9bfb0636d57965e90dd137aceef90a
Bobby Araiza <bobbyaraiza@gmail.com>
2019-01-21 00:11:47 -0800

    Update video-preview.js

--- src/components/video-preview.js
+++ src/components/video-preview.js
@@ -4,15 +4,16 @@ import Img from 'gatsby-image'
 
 import styles from './video-preview.module.css'
 
-export default ({ article }) => (
+export default ({ video }) => (
   <div className={styles.preview}>
+    <Img alt="" sizes={video.heroImage.sizes} />
     <h3 className={styles.previewTitle}>
-      <Link to={`/video/${article.slug}`}>{article.title}</Link>
+      <Link to={`/video/${video.slug}`}>{video.title}</Link>
     </h3>
-    <small>{article.publishDate}</small>
+    <small>{video.publishDate}</small>
     <p
       dangerouslySetInnerHTML={{
-        __html: article.description.childMarkdownRemark.html,
+        __html: video.description.childMarkdownRemark.html,
       }}
     />
   </div>
