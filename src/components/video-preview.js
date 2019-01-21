763ad76ea52e2c72f6ac87930c346f02f14aeece
Bobby Araiza <bobbyaraiza@gmail.com>
2019-01-20 23:54:45 -0800

    Update video-preview.js
      removed from line 9  <Img alt="" sizes={article.heroImage.sizes} />

--- src/components/video-preview.js
+++ src/components/video-preview.js
@@ -6,7 +6,6 @@ import styles from './video-preview.module.css'
 
 export default ({ article }) => (
   <div className={styles.preview}>
-    <Img alt="" sizes={article.heroImage.sizes} />
     <h3 className={styles.previewTitle}>
       <Link to={`/video/${article.slug}`}>{article.title}</Link>
     </h3>
